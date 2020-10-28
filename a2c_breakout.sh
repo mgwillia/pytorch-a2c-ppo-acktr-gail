f0bca7c06e741c59f0a94b54d1030f8c576fb600
mgwillia <mattgwilliamjr@byu.edu>
2020-10-28 01:36:50 -0600

    asdf

--- a2c_breakout.sh
+++ a2c_breakout.sh
@@ -8,11 +8,12 @@
 #SBATCH --time=8:00:00                                          # how long you think your job will take to complete; format=hh:mm:ss
 #SBATCH --qos=default                                           # set QOS, this will determine what resources can be requested
 #SBATCH --partition=class
-#SBATCH --mem=2gb                                               # memory required by job; if unit is not specified MB will be assumed
+#SBATCH --mem=8gb                                               # memory required by job; if unit is not specified MB will be assumed
 #SBATCH --gres=gpu:1
 
 module load cuda/10.1.243                                       # run any commands necessary to setup your environment
 
-srun --mem=2gb bash -c "hostname; python main.py --env-name=BreakoutNoFrameskip-v4"
+srun bash -c "hostname; python main.py --env-name=BreakoutNoFrameskip-v4 --log-dir=/fs/classhomes/fall2020/cmsc828w/c828w021/pytorch-a2c-ppo-acktr-gail/a2c_breakout_1/"
+#srun --mem=8gb bash -c "hostname; python main.py --env-name=BreakoutNoFrameskip-v4 --log-dir=/fs/classhomes/fall2020/cmsc828w/c828w021/pytorch-a2c-ppo-acktr-gail/a2c_breakout_1/"
 
 # once the end of the batch script is reached your job allocation will be revoked
