d503d19cdfc1c5b2e02577784147ae2b6828396f
mgwillia <mattgwilliamjr@byu.edu>
2020-10-28 17:08:23 -0600

    new batch stuff

--- a2c_beamrider_1.sh
+++ a2c_beamrider_1.sh
@@ -2,10 +2,10 @@
 
 # Lines that begin with #SBATCH specify commands to be used by SLURM for scheduling
 
-#SBATCH --job-name=a2c_beamrider                                 # sets the job name
-#SBATCH --output=a2c_beamrider.out.%j                            # indicates a file to redirect STDOUT to; %j is the jobid 
-#SBATCH --error=a2c_beamrider.out.%j                             # indicates a file to redirect STDERR to; %j is the jobid
-#SBATCH --time=8:00:00                                          # how long you think your job will take to complete; format=hh:mm:ss
+#SBATCH --job-name=a2c_beamrider_1                                 # sets the job name
+#SBATCH --output=a2c_beamrider_1.out.%j                            # indicates a file to redirect STDOUT to; %j is the jobid 
+#SBATCH --error=a2c_beamrider_1.out.%j                             # indicates a file to redirect STDERR to; %j is the jobid
+#SBATCH --time=5:00:00                                          # how long you think your job will take to complete; format=hh:mm:ss
 #SBATCH --qos=default                                           # set QOS, this will determine what resources can be requested
 #SBATCH --partition=class
 #SBATCH --mem=8gb                                               # memory required by job; if unit is not specified MB will be assumed
@@ -14,6 +14,5 @@
 module load cuda/10.1.243                                       # run any commands necessary to setup your environment
 
 srun bash -c "hostname; python main.py --env-name=BeamRiderNoFrameskip-v4 --log-dir=/fs/classhomes/fall2020/cmsc828w/c828w021/pytorch-a2c-ppo-acktr-gail/a2c_beamrider_1/"
-#srun --mem=8gb bash -c "hostname; python main.py --env-name=BreakoutNoFrameskip-v4 --log-dir=/fs/classhomes/fall2020/cmsc828w/c828w021/pytorch-a2c-ppo-acktr-gail/a2c_beamrider_1/"
 
 # once the end of the batch script is reached your job allocation will be revoked
---
+++ a2c_breakout_4.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Lines that begin with #SBATCH specify commands to be used by SLURM for scheduling
+
+#SBATCH --job-name=a2c_breakout_4                                 # sets the job name
+#SBATCH --output=a2c_breakout_4.out.%j                            # indicates a file to redirect STDOUT to; %j is the jobid 
+#SBATCH --error=a2c_breakout_4.out.%j                             # indicates a file to redirect STDERR to; %j is the jobid
+#SBATCH --time=5:00:00                                          # how long you think your job will take to complete; format=hh:mm:ss
+#SBATCH --qos=default                                           # set QOS, this will determine what resources can be requested
+#SBATCH --partition=class
+#SBATCH --mem=8gb                                               # memory required by job; if unit is not specified MB will be assumed
+#SBATCH --gres=gpu:1
+
+module load cuda/10.1.243                                       # run any commands necessary to setup your environment
+
+srun bash -c "hostname; python main.py --env-name=BreakoutNoFrameskip-v4 --log-dir=/fs/classhomes/fall2020/cmsc828w/c828w021/pytorch-a2c-ppo-acktr-gail/a2c_breakout_4/"
+
+# once the end of the batch script is reached your job allocation will be revoked
